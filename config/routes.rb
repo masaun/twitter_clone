d2aab5fd4b40c1462fc514a1e0db2ecb043ce216
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-08-04 10:14:41 +0900

    add_profile_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-
-  get 'users/show'
-
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
